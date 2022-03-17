53e36ecf9c5fb9b1fc37f04e9749e97ac95e1251
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-03-18 01:10:08 +0530

    Delete 1_call_execl.c

--- 1_call_execl.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<unistd.h>
-
-int main()
-{
-	execl("/home/mahima/mahima/einfochips/Final_project/linux_internals_assignment/process_assignment/1_exce_fd", "./1_exce_fd" , 0);
-
-	return 0;
-}
