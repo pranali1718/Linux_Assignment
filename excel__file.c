518aa59c1ad39eb9389f9b53d87023e892cd7191
pranali1718 <98475530+pranali1718@users.noreply.github.com>
2022-03-24 17:47:47 +0530

    Rename fd_execl.c to excel__file.c

---
+++ excel__file.c
@@ -0,0 +1,1 @@
+Test whether the process(exec() system call) that replaces old program data , will inherit the fd's or not
--- fd_execl.c
+++
@@ -1,1 +0,0 @@
-Test whether the process(exec() system call) that replaces old program data , will inherit the fd's or not
